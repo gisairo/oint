984e0b2aeac74349b9207cd75759688530a5ffd7
Brian Gisairo Otwoma <jamesgsr@gmail.com>
2016-08-01 23:49:34 +0300

    added readme

---
+++ README.md
@@ -0,0 +1,2 @@
+*CHROME EXTENSION WHICH OPENS CURRENT URL IN NEW TAB*
+Very handy for sites that use cookies to track how many articles you have read
---
+++ background.js
@@ -0,0 +1,19 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    // chrome.windows.create({"url": url, "incognito": true});
+    // console.log(activeTab);
+  });
+
+});
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "open_new_tab" ) {
+      // chrome.tabs.create({"url": request.url});
+      chrome.windows.create({"url": request.url, "incognito": true});
+    }
+  }
+);
